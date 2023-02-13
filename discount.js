0e812ab8ea41f8d8d376e598d27cc3b515fd9724
Fakrul Islam <dev.rupok02@gmail.com>
2023-02-13 15:50:55 +0600

    Discount Cricket ticket

---
+++ discount.js
@@ -0,0 +1,28 @@
+function ticketPrice(ticketQuantity){
+    const first100Rate = 100;
+    const second100Rate = 90;
+    const restTicketRate = 70;
+
+    if(ticketQuantity <= 100){
+        const price = ticketQuantity * first100Rate;
+        return price;
+    }
+    else if(ticketQuantity <= 200){
+        const first100Price = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
+    }
+    else{
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity * restTicketRate;
+        const totalCost = first100Price + second100Price + restTicketPrice;
+        return totalCost;
+    }
+}
+
+const price = ticketPrice(210);
+console.log('Price: ',price);
